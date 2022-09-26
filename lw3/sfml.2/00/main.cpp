b9e529d6ebc0bd7975797c6cace81f1c380acc4d
Aleksandra Andreeva <shu-@list.ru>
2022-09-26 21:55:22 +0300

    Added flying ball

---
+++ lw3/sfml.2/00/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    for (int num = 1; num <= 100; num += 1)
+    {
+        if ((num % 3 == 0) && (num % 5 == 0))
+        {
+            std::cout << "FizzBuzz" << std::endl;
+        }
+        else if (num % 3 == 0)
+        {
+            std::cout << "Fizz" << std::endl;
+        }
+        else if (num % 5 == 0)
+        {
+            std::cout << "Buzz" << std::endl;
+        }
+        else
+        {
+            std::cout << num << std::endl;
+        }
+    }
+}
